3cc43d8e55660141dd0c1899ebb6f757ca5f1f77
Hackerpilot <briancschott@gmail.com>
2013-05-06 00:40:30 +0000

    Printer rewrite

--- graphgen.d
+++ graphgen.d
@@ -5,133 +5,192 @@ import std.conv;
 import std.ascii;
 import std.string;
 
-class Rule
+class Node
+{
+	string id;
+	abstract void assignIds(string id);
+	abstract void print(string[] startIds);
+	abstract string[] getStartIds();
+	abstract string[] getEndIds();
+}
+
+class RuleDefinition : Node
 {
 	string name;
-	SubRule subRule;
-	string nodeName;
+	Alternatives alternatives;
 
-	void assignId(string id)
+	override void assignIds(string id)
 	{
-		nodeName = id;
-		subRule.id = format("%s_0", id);
-		subRule.assignIds();
+		this.id = id;
+		alternatives.assignIds(id);
 	}
 
-	void print()
+	override void print(string[] startIds)
 	{
-		const string startName = nodeName ~ "start";
-		const string endName = nodeName ~ "end";
-		printDotNode(startName);
-		printDotNode(endName);
-		subRule.print([startName]);
-//			foreach (sr; subRule.firstIds())
-//				printArrow(startName, sr);
-		foreach (sr; subRule.lastIds())
-			printArrow(sr, endName);
+		string startDot = format("%s_start", id);
+		string endDot = format("%s_end", id);
+		alternatives.print([startDot]);
+		foreach (end; alternatives.getEndIds())
+			printArrow(end, endDot);
+	}
+
+	override string[] getStartIds() { return null; }
+	override string[] getEndIds() { return null; }
+}
+
+class Alternatives : Node
+{
+	Node[] alternatives;
+
+	override void assignIds(string id)
+	{
+		foreach (i, alt; alternatives)
+			alt.assignIds(format("%s_%d", id, i));
+	}
+
+	override void print(string[] startIds)
+	{
+		foreach (alt; alternatives)
+		{
+			alt.print(startIds);
+			foreach (start; startIds)
+				printArrow(start, alt.id);
+		}
+	}
+
+	override string[] getStartIds()
+	{
+		string[] startIds;
+		foreach (alt; alternatives)
+		{
+			startIds ~= alt.getStartIds();
+		}
+		return startIds;
+	}
+
+	override string[] getEndIds()
+	{
+		string[] endIds;
+		foreach (alt; alternatives)
+		{
+			endIds ~= alt.getEndIds();
+		}
+		return endIds;
 	}
 }
 
-class SubRule
+class Sequence : Node
 {
+	Node[] items;
+}
+
+class Terminal : Node
+{
+	string terminal;
+
+	override void assignIds(string id)
+	{
+		this.id = id;
+	}
+
+	override void print(string[] startIds)
+	{
+		writeln(id, `[shape=oval, label="`, terminal.replace(`"`, `\"`), `"]`);
+		foreach (s; startIds)
+			printArrow(s, id);
+	}
+
+	override string[] getStartIds() { return [id]; }
+
+	override string[] getEndIds() { return [id]; }
+}
+
+class RuleReference : Node
+{
+	string ruleReference;
+
+	override void assignIds(string id)
+	{
+		this.id = id;
+	}
+
+	override void print(string[] startIds)
+	{
+		writeln(id, `[shape=rectangle, label="`, ruleReference.replace(`"`, `\"`), `"]`);
+		foreach (s; startIds)
+			printArrow(s, id);
+	}
+
+	override string[] getStartIds() { return [id]; }
+
+	override string[] getEndIds() { return [id]; }
+}
+
+class OptionOrRepeat : Node
+{
+	Node node;
 	Qualifier qualifier;
-	SubRule[] parts;
-	string termOrRule;
-	SubRuleType type;
-	string id;
+	string repeatStart;
+	string repeatEnd;
+	string optionStart;
+	string optionEnd;
 
-	void print(string[] startIds)
+	override void print(string[] startIds)
 	{
-		if (type == SubRuleType.terminal || type == SubRuleType.rule)
+		final switch (qualifier)
 		{
-			if (type == SubRuleType.terminal)
-				printTerminalNode(id, termOrRule);
-			if (type == SubRuleType.rule)
-				printRuleNode(id, termOrRule);
-			foreach (startId; startIds)
-				printArrow(startId, id);
-		}
-		else if (type == SubRuleType.alternatives)
-		{
-			foreach (sub; parts)
-				sub.print(startIds);
-		}
-		else if (type == SubRuleType.sequence)
-		{
-			foreach (i; 0 .. parts.length)
-			{
-				auto sub = parts[i];
-				if (i == 0)
-					sub.print(startIds);
-				else
-					sub.print(parts[i - 1].lastIds());
-			}
+			case Qualifier.star:
+				foreach (start; startIds)
+					printArrow(start, optionStart);
+				printArrow(optionStart, repeatStart);
+				node.print([repeatStart]);
+				foreach (end; node.getEndIds())
+					printArrow(end, repeatEnd);
+				printArrow(repeatEnd, repeatStart);
+				printArrow(repeatEnd, optionEnd);
+				printArrow(optionStart, optionEnd);
+				break;
+			case Qualifier.question:
+				foreach (start; startIds)
+					printArrow(start, optionStart);
+				printArrow(optionStart, optionEnd);
+				node.print([optionStart]);
+				foreach (end; node.getEndIds())
+					printArrow(end, optionEnd);
+				break;
+			case Qualifier.plus:
+				foreach (start; startIds)
+					printArrow(start, repeatStart);
+				node.print([repeatStart]);
+				foreach (end; node.getEndIds())
+					printArrow(end, repeatEnd);
+				break;
 		}
 	}
 
-	void assignIds()
+	override string[] getStartIds()
 	{
-		foreach (i, part; parts)
-		{
-			part.id = format("%s_%d", id, i);
-			part.assignIds();
-		}
+		if (qualifier == Qualifier.star || qualifier == Qualifier.question)
+			return [optionStart];
+		else
+			return [repeatStart];
 	}
 
-	string[] firstIds()
+	override string[] getEndIds()
 	{
-		final switch (type)
-		{
-		case SubRuleType.invalid:
-			assert (false, "invalid Node");
-		case SubRuleType.terminal:
-		case SubRuleType.rule:
-			return [id];
-		case SubRuleType.alternatives:
-			string[] ids;
-			foreach (rule; parts)
-				ids ~= rule.firstIds();
-			return ids;
-		case SubRuleType.sequence:
-			return parts[0].firstIds();
-		}
+		if (qualifier == Qualifier.star || qualifier == Qualifier.question)
+			return [optionEnd];
+		else
+			return [repeatEnd];
 	}
 
-	string[] lastIds()
-	{
-		final switch (type)
-		{
-		case SubRuleType.invalid:
-			assert (false, "invalid Node " ~ termOrRule);
-		case SubRuleType.terminal:
-		case SubRuleType.rule:
-			return [id];
-		case SubRuleType.alternatives:
-			string[] ids;
-			foreach (rule; parts)
-				ids ~= rule.lastIds();
-			return ids;
-		case SubRuleType.sequence:
-			return parts[$ - 1].lastIds();
-		}
-	}
 }
 
 enum Qualifier
 {
-	none,
-	plus,
 	star,
-	question
-}
-
-enum SubRuleType
-{
-	invalid,
-	terminal,
-	rule,
-	sequence,
-	alternatives,
+	question,
+	plus
 }
 
 void main(string[] args)
@@ -150,68 +209,11 @@ void main(string[] args)
 
 	for (int i = 0; !tokens.empty; i++)
 	{
-		auto rule = parseRule(tokens);
-		rule.assignId(format("rule_%d", i));
-		rule.print();
 	}
 
 	writeln("}");
 }
 
-Rule parseRule(T)(ref T tokens)
-{
-	Rule r = new Rule;
-	r.name = tokens.front().value;
-	tokens.popFront();
-	assert (tokens.front == TokenType.colon, tokens.front.value);
-	tokens.popFront();
-	r.subRule = parseSubRule(tokens);
-	return r;
-}
-
-SubRule parseSubRule(T)(ref T tokens)
-{
-	SubRule rule = new SubRule;
-
-	while (true)
-	{
-		if (tokens.empty || tokens.front == TokenType.rParen
-			|| tokens.front == TokenType.semicolon)
-		{
-			return rule;
-		}
-
-		if (tokens.front == TokenType.identifier)
-		{
-			auto r = new SubRule;
-			r.type == SubRuleType.rule;
-			r.termOrRule = tokens.front.value;
-		}
-	}
-	if (tokens.front == TokenType.identifier)
-	{
-
-	}
-
-	return rule;
-}
-
-
-void printTerminalNode(string nodeName, string terminal)
-in
-{
-	assert(nodeName.length > 0);
-	assert(terminal.length > 0);
-}
-body
-{
-	writeln(nodeName, `[shape=oval, label="`, terminal.replace(`"`, `\"`), `"]`);
-}
-
-void printRuleNode(string nodeName, string ruleName)
-{
-	writeln(nodeName, `[shape=rectangle, label="`, ruleName.replace(`"`, `\"`), `"]`);
-}
 
 void printDotNode(string nodeName)
 {
